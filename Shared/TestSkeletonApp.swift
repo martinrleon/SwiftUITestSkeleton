47c744f93454c074f27ee105e09159c67d2857be
Martin Leon <martinrleon@yahoo.com>
2021-09-03 12:57:30 -0500

    Add starter project

---
+++ Shared/TestSkeletonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSkeletonApp.swift
+//  Shared
+//
+//  Created by Martin R Leon on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TestSkeletonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
